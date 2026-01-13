baf0dbb4505de56b7082b6bc1abf84b32ec406ee
fabinhocl <fabiocasadolima@gmail.com>
2026-01-12 23:50:47 -0300

    ajuste no rótulo de ensino infantil e criação de migração para alteração do campo 'ensino' no modelo Aluno

--- AppLSD/models.py
+++ AppLSD/models.py
@@ -416,7 +416,7 @@ class Aluno(AuditModel):
     school = models.CharField(max_length=200, default='', blank=True)
     ENSINO_CHOICES = [
     ('', 'Selecione o ensino'),  # Django usa por padrão esse rótulo se vazio
-    ('infantil', 'Alfabetização'),
+    ('infantil', 'Ensino Infantil'),
     ('fundamental1', 'Ensino Fundamental 1'),
     ('fundamental2', 'Ensino Fundamental 2'),
     ('medio', 'Ensino Médio'),
